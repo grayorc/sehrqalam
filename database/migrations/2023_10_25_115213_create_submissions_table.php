159419b10ba7abe46b623bf1c036dbec40884d65
alireza10up <toolsalireza10up@gmail.com>
2023-12-17 16:05:23 +0330

    Fix Database

--- database/migrations/2023_10_25_115213_create_submissions_table.php
+++ database/migrations/2023_10_25_115213_create_submissions_table.php
@@ -4,13 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
+
+        Schema::create('age_category', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('label');
+        });
+
         Schema::create('submissions', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
@@ -22,7 +28,7 @@ return new class extends Migration
             $table->string('secondary_title');
             $table->string('english_title');
             $table->unsignedBigInteger('category_id');
-             $table->unsignedBigInteger('age_category_id');
+            $table->unsignedBigInteger('age_category_id');
             $table->string('word_number');
             $table->boolean('editing');
             $table->boolean('cover_design');
@@ -34,17 +40,17 @@ return new class extends Migration
             $table->string('code_meli');
             $table->string('code_posti');
             $table->string('phone_number');
-             $table->unsignedBigInteger('shahr_id');
-             $table->unsignedBigInteger('ostan_id');
+            $table->unsignedBigInteger('shahr_id');
+            $table->unsignedBigInteger('ostan_id');
             //
             $table->timestamp('closed_at')->nullable();
             $table->timestamps();
             //references
             $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('category_id')->references('id')->on('categories');
-             $table->foreign('age_category_id')->references('id')->on('age_category');
-             $table->foreign('shahr_id')->references('id')->on('shahr');
-             $table->foreign('ostan_id')->references('id')->on('ostan');
+            $table->foreign('age_category_id')->references('id')->on('age_category');
+            $table->foreign('shahr_id')->references('id')->on('shahr');
+            $table->foreign('ostan_id')->references('id')->on('ostan');
         });
 
         Schema::create('writers', function (Blueprint $table) {
@@ -65,12 +71,6 @@ return new class extends Migration
 
             //
             $table->foreign('submission_id')->references('id')->on('submissions');
-        });
-
-        Schema::create('age_category', function (Blueprint $table) {
-            $table->id();
-            $table->string('name')->unique();
-            $table->string('label');
         });
     }
 
