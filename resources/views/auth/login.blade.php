3c8f16efa8da4f046244f649fc21ffdbc3fec570
grayorc <gray.orc.80@gmail.com>
2023-12-24 08:28:24 +0330

    update login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>لاگین</title>
+    <title>ورود</title>
     <link rel="stylesheet" href="{{ asset('loginPage/style/login.css') }}">
     <style>
         .capcha-span img{
